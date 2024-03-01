3cad32e092ff05b28fef9f55ceb148d8b3569bfb
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2024-03-01 13:03:56 +0530

    leap year

--- practice.js
+++ practice.js
@@ -17,80 +17,90 @@
 // console.log(result)
 
 // q2 Write a program that checks if a number is even or odd.
-function EvenOdd(num) {
-  if(num%2 == 0){
-     console.log("given number is even")
-  } else {
-    console.log("given number is odd")
-  }
-}
-EvenOdd(1011)
+// function EvenOdd(num) {
+//   if(num%2 == 0){
+//      console.log("given number is even")
+//   } else {
+//     console.log("given number is odd")
+//   }
+// }
+// EvenOdd(1011)
 
-// q3 Write a program to determine the greater of two numbers.
-function TwoGreaterNum(a,b) {
-    if(a>b && b<a) {
-        console.log("a is greater")
-    }
-    else if(b>a && a<b){
-        console.log("b is greater")
-    }
+// // q3 Write a program to determine the greater of two numbers.
+// function TwoGreaterNum(a,b) {
+//     if(a>b && b<a) {
+//         console.log("a is greater")
+//     }
+//     else if(b>a && a<b){
+//         console.log("b is greater")
+//     }
 
-}
-TwoGreaterNum(157, 117)
+// }
+// TwoGreaterNum(157, 117)
 
-// question 4 Write a program that transforms a numerical grade to a letter grade (e.g grade 10 should display “A”).
-function Transnumtletter(num) {
+// // question 4 Write a program that transforms a numerical grade to a letter grade (e.g grade 10 should display “A”).
+// function Transnumtletter(num) {
    
-    let grade;
-    if(num >= 90){
-        console.log("grade A")
-        grade = 'A';
-    }
-    else if(num >= 80) {
-        console.log("gradeB")
-        grade = 'B'
-    }
-    else if(num >= 70){
-        console.log("grade C")
-        grade = 'C'
-    }
-    else if(num >= 60) {
-        console.log("grade D")
-        grade = 'D'
-    }
-    else if(num >= 50) {
-        console.log("grade F")
-        grade = 'F'
-    }
-    else {
-        grade = 'Fail'
-    }
-    console.log("grade is :" + grade)
+//     let grade;
+//     if(num >= 90){
+//         console.log("grade A")
+//         grade = 'A';
+//     }
+//     else if(num >= 80) {
+//         console.log("gradeB")
+//         grade = 'B'
+//     }
+//     else if(num >= 70){
+//         console.log("grade C")
+//         grade = 'C'
+//     }
+//     else if(num >= 60) {
+//         console.log("grade D")
+//         grade = 'D'
+//     }
+//     else if(num >= 50) {
+//         console.log("grade F")
+//         grade = 'F'
+//     }
+//     else {
+//         grade = 'Fail'
+//     }
+//     console.log("grade is :" + grade)
 
    
-}
-// Transnumtletter(89) 
+// }
+// // Transnumtletter(89) 
 
-// qwestion 5 Write a program that calculates the ticket price based on age with the following conditions: age below 12 pay a ticket price of 5, age below 18 pay a ticket price of 10, age below 60 pay a ticket price of 20, age over 60 play a ticket price of 15.
-function TicketPrice(age) {
-    var Rupess;
-    if(age < 12){
-    Rupess = 5
-    console.log(Rupess)
-    }
-    else if(age < 18) {
-        Rupess = 10
-    }
-    else if (age < 60) {
-        Rupess = 20
-    }
+// // qwestion 5 Write a program that calculates the ticket price based on age with the following conditions: age below 12 pay a ticket price of 5, age below 18 pay a ticket price of 10, age below 60 pay a ticket price of 20, age over 60 play a ticket price of 15.
+// function TicketPrice(age) {
+//     var Rupess;
+//     if(age < 12){
+//     Rupess = 5
+//     console.log(Rupess)
+//     }
+//     else if(age < 18) {
+//         Rupess = 10
+//     }
+//     else if (age < 60) {
+//         Rupess = 20
+//     }
 
-    else if(age > 60) {
-        Rupess = 15
-    }
+//     else if(age > 60) {
+//         Rupess = 15
+//     }
  
-console.log(Rupess)
+// console.log(Rupess)
+// }
+
+// TicketPrice(5);
+// Write a program that determines if a year is a leap year.
+function leapYear(year) {
+   let result;
+    if(year%4 == 0 && (year % 100 !== 0 || year % 400 === 0)){
+result = 'Year is leap year'
+    } else {
+  result = 'Year is not leap year'
+    }
+    console.log(result)
 }
-
-TicketPrice(5);
-
+leapYear(2024)
