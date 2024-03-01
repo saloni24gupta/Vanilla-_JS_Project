01101272a4c5ceede5d035a83a07f38e11f7f093
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2024-03-01 12:31:09 +0530

    question3

--- practice.js
+++ practice.js
@@ -1,19 +1,27 @@
 // q1 Write a program that determines whether a given number is positive or negative.
-a = prompt("Enter the number");
-a = Number.parseInt(a)
-let result;
-function positiveNegative(a){
-if(a>0){
-    result = 'positive'
-    console.log("given number is positive");
-} 
-else {
-    result = 'negative'
-    console.log("given number is negative")
-}
+// a = prompt("Enter the number");
+// a = Number.parseInt(a)
+// let result;
+// function positiveNegative(a){
+// if(a>0){
+//     result = 'positive'
+//     console.log("given number is positive");
+// } 
+// else {
+//     result = 'negative'
+//     console.log("given number is negative")
+// }
 
-}
-positiveNegative(a)
-console.log(result)
+// }
+// positiveNegative(a)
+// console.log(result)
 
 // q2 Write a program that checks if a number is even or odd.
+function EvenOdd(num) {
+  if(num%2 == 0){
+     console.log("given number is even")
+  } else {
+    console.log("given number is odd")
+  }
+}
+EvenOdd(1011)
