097f53643aaae23075301d1a67344583beb08f16
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2024-03-09 21:01:04 +0530

    event hancler

--- handler.js
+++ handler.js
@@ -1,4 +1,14 @@
-var BBTN = document.getElementById("Btn")
-BBTN.addEventListener('click', function() {
-    alert("hellowelr")
-})
+
+let BBTN = document.getElementById("Btn")
+let x = function(e) {
+    alert("hELLO WORLD");
+}
+
+let y = function(e) {
+    alert("hELLO WORLD1");
+}
+
+
+BBTN.addEventListener('click', y) 
+
+BBTN.removeEventListener('click',x )
